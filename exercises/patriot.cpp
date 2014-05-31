6fcc5fe54e9ddd7b0e6ec49a2a1beaa95f0a566b
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2014-05-31 16:08:40 +0200

    add rounding

--- exercises/patriot.cpp
+++ exercises/patriot.cpp
@@ -1,12 +1,25 @@
 #include<cstdio>
-
+#include<fenv.h>
 
 int main() {
+
+/*
+FE_TONEAREST //Round to nearest.
+FE_UPWARD    // Round toward +&infin;.
+FE_DOWNWARD // Round toward -&infin;.
+FE_TOWARDZERO //
+*/
+
+   printf("rounding %d\n",fegetround());
+//   fesetround(FE_UPWARD);
+//   fesetround(FE_TOWARDZERO);
+   printf("new rounding %d\n",fegetround());
+
    float tenth=0.1f;
    float t=0;
    long long n=0;
    while(n<1000000) {
-     t+=0.1f;
+     t+=tenth;
      ++n;
      if (n<21 || n%36000==0) printf("%d %f %a\n",n,t,t);
    }
